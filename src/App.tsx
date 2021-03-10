2097f4f3a800dbd6cd6659f6f29a6af72a70f765
Mitch Warrenburg <mitch_warrenburg@keybank.com>
2021-03-10 01:51:08 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>SHEEIIITT</h1>
+      wow
+    </div>
+  );
+};
+
+App.defaultProps = {};
+
+export default App;
