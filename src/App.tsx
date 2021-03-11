beed79e511ccc1897c68f9a3d2fa72e52128fbe5
Mitch Warrenburg <mitch_warrenburg@keybank.com>
2021-03-10 19:45:28 -0800

    Finishes intro animation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useEffect } from 'react';
+import Particles from 'react-tsparticles';
 import lastNameImage from './assets/lastname.png';
 import firstNameImage from './assets/firstname.png';
 
@@ -39,6 +40,117 @@ const App: FC = () => {
 
   return (
     <div className="intro-screen">
+      <Particles
+        id="stars-background"
+        className="intro-screen__stars-background"
+        init={(main: any) => console.log('main:', main)}
+        loaded={(container: any) => console.log('loaded:', container)}
+        options={{
+          particles: {
+            number: {
+              value: 550,
+              density: {
+                enable: true,
+                value_area: 790,
+              },
+            },
+            color: {
+              value: '#ffffff',
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 0,
+                color: '#000000',
+              },
+              polygon: {
+                nb_sides: 5,
+              },
+            },
+            opacity: {
+              value: 0.75,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 0.2,
+                opacity_min: 0.2,
+                sync: false,
+              },
+            },
+            size: {
+              value: 2,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 2,
+                size_min: 0,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: false,
+              distance: 150,
+              color: '#ffffff',
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 0.1,
+              direction: 'none',
+              random: true,
+              straight: false,
+              out_mode: 'out',
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
+          },
+          interactivity: {
+            detect_on: 'canvas',
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'bubble',
+              },
+              onclick: {
+                enable: true,
+                mode: 'push',
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                line_linked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 83.91608391608392,
+                size: 1,
+                duration: 3,
+                opacity: 1,
+                speed: 3,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
+          },
+          retina_detect: true,
+        }}
+      />
       <section className="animated-intro-section">
         <img
           src={firstNameImage}
